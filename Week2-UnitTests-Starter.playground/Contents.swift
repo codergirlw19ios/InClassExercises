a82e074f9d37861685e406e6e39e953c2ad8a146
Amanda Rawls <22384389+amandarawls@users.noreply.github.com>
2019-02-06 20:03:52 -0600

    added assignment

--- Week2-UnitTests-Starter.playground/Contents.swift
+++ Week2-UnitTests-Starter.playground/Contents.swift
@@ -6,8 +6,38 @@ func twoPlusTwo() -> Int {
     return 2 + 2
 }
 
+func returnInitials(from string: String) -> String {
+    let words = string.components(separatedBy: " ")
+    var initials = [String]()
+    for word in words {
+        initials.append(String(word.first!).capitalized)
+    }
+    
+    return initials.joined()
+}
+
+// week
+// solve FizzBuzz
+// find index of first numeric character in a string
+
+let string = "B0b has a typo problem."
+
+let arrayOfCharacters = Array(string)
+var indicesOfNumerics = [Int]()
+for (index, character) in arrayOfCharacters.enumerated() {
+    guard let _ = Int(String(character)) else { continue }
+    indicesOfNumerics.append(index)
+}
+
+let firstNumeric = indicesOfNumerics.first
 
 class UnitTests: XCTestCase {
+    func testReturnInitials(){
+        let testInput = "nathen Is Cool"
+        let expectedOutput = "NIC"
+        let actualOutput = returnInitials(from: testInput)
+        XCTAssertEqual(expectedOutput, actualOutput)
+    }
     
     func testTwoPlusTwo() {
         let expectedOutput = 4
---
+++ Week2Week3Assignment.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+//PROBLEM 1:
+// Week 2 problem
+// Fizz Buzz:
+// with if/else statements and with switch statement
+// given a number, if the number is divisible by 3, print "Fizz"
+// if the number is divisible by 5, print "Buzz"
+// if the number is divisible by 3 and 5, print "FizzBuzz"
+// otherwise, print the number
+
+// week 3 problem:
+// FizzBuzz part 2:
+// write a function that takes an array of numbers and prints fizzbuzz for that array
+
+
+// PROBLEM 2:
+// week 2 problem:
+let string = "B0b has a typo problem."
+// expectedOutput = 1
+
+// Tell me what the index is of the first numeric character is. Your solution should work for any string. Give me the index in the form of an integer.
+
+// Week 3 problem
+// Give me the the String.Index as a result of using the firstIndex(where:) method.
+
+
+// PROBLEM 3:
+// week 2 problem:
+
+let string2 = "Nathen is Cool"
+
+//let expectedOutput = "NIC"
+
+// give me the first letter of each word in the sentence back in a single string, with all the letters capitalized, with no spaces in th resulting string.
+
+// week 3 problem:
+
+// write a function that will take a 2D array of strings and return a 2d array of strings.
+// input example: ["Nathen is Cool", "Amanda is kinda weird"]
+// expected output: ["NIC", "AIKW"]
